3e5f063850c8ce29c34e46d1a2c3efaf4acac394
evie <e.v.221b@gmail.com>
2024-07-18 21:43:08 +0330

    till sesstion 014

--- script.js
+++ script.js
@@ -3,16 +3,15 @@
 // import { addToCard, totalPrice as price, qt } from "./shoppingCard.js";
 // addToCard("bread", 5);
 // console.log(price, qt);
-
+*/
 console.log("importing module");
-
+/*
 import * as shoppinCart from "./shoppingCard.js";
 shoppinCart.addToCard("bread", 5);
-
 // dont do this : importing defult and named exports
 // import add, { addToCard, totalPrice as price, qt } from "./shoppingCard.js";
 // console.log(price);
-
+*/
 import add, { cart } from "./shoppingCard.js";
 add("pizza", 2);
 add("appels", 2);
@@ -22,7 +21,7 @@ add("fries", 2);
 // they are not the copy of them
 // they point to the same thing
 console.log(cart);
-
+/*
 //////////// SECTION TOP LEVEL AWAIT
 
 // console.log("start fetching");
@@ -69,3 +68,42 @@ const shoppingcart2 = (function () {
 })();
 shoppingcart2.addToCard("appeles", 2);
 */
+// import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+import cloneDeep from "lodash-es";
+
+const state = {
+  cart: [
+    { product: "bread", quantity: 5 },
+    { product: "pizza", quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+console.log(stateClone);
+
+const stateCloneDeep = cloneDeep(state);
+
+state.user.loggedIn = false;
+
+console.log(stateCloneDeep);
+
+if (module.hot) {
+  module.hot.accept();
+}
+class person {
+  #greeding = "Hey";
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeding},${this.name}`);
+  }
+}
+const Evie = new person("Evie");
+
+console.log("Evie" ?? null);
+
+console.log(cart.find((el) => el.quantity >= 2));
+Promise.resolve("test").then((x) => console.log(x));
+
+import "core-js/stable";
+// pollyfilling async functions
+import "regenerator-runtime/runtime";
